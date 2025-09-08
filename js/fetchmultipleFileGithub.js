739cb77836fa0805672f75daae93e1dae66bc043
cerongithub <developerceron@gmail.com>
2025-09-08 08:41:26 +0800

    dynamic calender 50percent done

--- js/fetchmultipleFileGithub.js
+++ js/fetchmultipleFileGithub.js
@@ -49,8 +49,21 @@ async function fetchTextFilesFromBranch() {
 }
 
 var toArray = [];
-const containerDiv = document.querySelector('.dynamicHere');
-containerDiv.innerHTML = "";
+const dynamicInput = document.querySelector('.inputDynamicHere');
+
+const dynamicCalendar = document.querySelector('.calendarHere');
+const dynamicDatesJan = document.querySelector('.JanDatesHere');
+const dynamicDatesFeb = document.querySelector('.FebDatesHere');
+const dynamicDatesMar = document.querySelector('.MarDatesHere');
+const dynamicMonthsDates = document.querySelector('.manipulationHere');
+
+dynamicInput.innerHTML = "";
+
+dynamicDatesJan.innerHTML = "";
+dynamicDatesFeb.innerHTML = "";
+dynamicDatesMar.innerHTML = "";
+dynamicMonthsDates.innerHTML = "";
+
 // Example usage
 fetchTextFilesFromBranch()
   .then(files => {
@@ -71,100 +84,195 @@ fetchTextFilesFromBranch()
         const newInputDP = document.createElement('input');
         const newInputBalance = document.createElement('input');
         const newInputComment = document.createElement('textarea');
+        const newInputDataCreated = document.createElement('input');
         
         // Set attributes for the input
         newInputName.setAttribute('type', 'text');
         newInputName.setAttribute('name', 'dynamicInputName_' + i);
         newInputName.setAttribute('id', 'dynamicInputName_' + i);
         newInputName.setAttribute('placeholder', 'Name: ' + (toJson[0]));
-        newInputName.setAttribute('style', 'width:150px;');
+        newInputName.setAttribute('style', 'width:150px; visibility:hidden');
         newInputName.setAttribute('value', (toJson[0]));
 
         newInputDateIn.setAttribute('type', 'text');
         newInputDateIn.setAttribute('name', 'dynamicInputDateIn_' + i);
         newInputDateIn.setAttribute('id', 'dynamicInputDateIn_' + i);
         newInputDateIn.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputDateIn.setAttribute('style', 'width:150px;');
+        newInputDateIn.setAttribute('style', 'width:150px; visibility:hidden');
         newInputDateIn.setAttribute('value', (toJson[1]));
 
         newInputDateOut.setAttribute('type', 'text');
         newInputDateOut.setAttribute('name', 'dynamicInputDateOut_' + i);
         newInputDateOut.setAttribute('id', 'dynamicInputDateOut_' + i);
         newInputDateOut.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputDateOut.setAttribute('style', 'width:150px;');
+        newInputDateOut.setAttribute('style', 'width:150px; visibility:hidden');
         newInputDateOut.setAttribute('value', (toJson[2]));
 
         newInputPhoneNumber.setAttribute('type', 'text');
         newInputPhoneNumber.setAttribute('name', 'dynamicInputPhoneNumber_' + i);
         newInputPhoneNumber.setAttribute('id', 'dynamicInputPhoneNumber_' + i);
         newInputPhoneNumber.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputPhoneNumber.setAttribute('style', 'width:150px;');
+        newInputPhoneNumber.setAttribute('style', 'width:150px; visibility:hidden');
         newInputPhoneNumber.setAttribute('value', (toJson[3]));
 
         newInputEmail.setAttribute('type', 'text');
         newInputEmail.setAttribute('name', 'dynamicInputEmail_' + i);
         newInputEmail.setAttribute('id', 'dynamicInputEmail_' + i);
         newInputEmail.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputEmail.setAttribute('style', 'width:150px;');
+        newInputEmail.setAttribute('style', 'width:150px; visibility:hidden');
         newInputEmail.setAttribute('value', (toJson[4]));
 
         newInputPax.setAttribute('type', 'text');
         newInputPax.setAttribute('name', 'dynamicInputPax_' + i);
         newInputPax.setAttribute('id', 'dynamicInputPax_' + i);
         newInputPax.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputPax.setAttribute('style', 'width:150px;');
+        newInputPax.setAttribute('style', 'width:150px; visibility:hidden');
         newInputPax.setAttribute('value', (toJson[5]));
 
         newInputExtraPax.setAttribute('type', 'text');
         newInputExtraPax.setAttribute('name', 'dynamicInputExtraPax_' + i);
         newInputExtraPax.setAttribute('id', 'dynamicInputExtraPax_' + i);
         newInputExtraPax.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputExtraPax.setAttribute('style', 'width:150px;');
+        newInputExtraPax.setAttribute('style', 'width:150px; visibility:hidden');
         newInputExtraPax.setAttribute('value', 0);
 
         newInputPet.setAttribute('type', 'text');
         newInputPet.setAttribute('name', 'dynamicInputPet_' + i);
         newInputPet.setAttribute('id', 'dynamicInputPet_' + i);
         newInputPet.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputPet.setAttribute('style', 'width:150px;');
+        newInputPet.setAttribute('style', 'width:150px; visibility:hidden');
         newInputPet.setAttribute('value', (toJson[6]));
 
         newInputPackage.setAttribute('type', 'text');
         newInputPackage.setAttribute('name', 'dynamicInputExtraPax_' + i);
         newInputPackage.setAttribute('id', 'dynamicInputExtraPax_' + i);
         newInputPackage.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputPackage.setAttribute('style', 'width:150px;');
+        newInputPackage.setAttribute('style', 'width:150px; visibility:hidden');
         newInputPackage.setAttribute('value', (toJson[7]));
 
         newInputcheckIn.setAttribute('type', 'text');
         newInputcheckIn.setAttribute('name', 'dynamicInputExtraPax_' + i);
         newInputcheckIn.setAttribute('id', 'dynamicInputExtraPax_' + i);
         newInputcheckIn.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputcheckIn.setAttribute('style', 'width:150px;');
-        newInputcheckIn.setAttribute('value', (toJson[7]));
+        newInputcheckIn.setAttribute('style', 'width:150px; visibility:hidden');
+        newInputcheckIn.setAttribute('value', (toJson[8]));
 
         newInputCheckOut.setAttribute('type', 'text');
         newInputCheckOut.setAttribute('name', 'dynamicInputExtraPax_' + i);
         newInputCheckOut.setAttribute('id', 'dynamicInputExtraPax_' + i);
         newInputCheckOut.setAttribute('placeholder', 'Name: ' + (toJson[1]));
-        newInputCheckOut.setAttribute('style', 'width:150px;');
-        newInputCheckOut.setAttribute('value', (toJson[7]));
+        newInputCheckOut.setAttribute('style', 'width:150px; visibility:hidden');
+        newInputCheckOut.setAttribute('value', (toJson[9]));
+
+        newInputDP.setAttribute('type', 'text');
+        newInputDP.setAttribute('name', 'dynamicInputExtraPax_' + i);
+        newInputDP.setAttribute('id', 'dynamicInputExtraPax_' + i);
+        newInputDP.setAttribute('placeholder', 'Name: ' + (toJson[1]));
+        newInputDP.setAttribute('style', 'width:150px; visibility:hidden');
+        newInputDP.setAttribute('value', (toJson[10]));
         
-        containerDiv.appendChild(newInputName);
-        containerDiv.appendChild(newInputDateIn);
-        containerDiv.appendChild(newInputDateOut);        
-        containerDiv.appendChild(newInputPhoneNumber);
-        containerDiv.appendChild(newInputEmail);
-        containerDiv.appendChild(newInputPax);
-        containerDiv.appendChild(newInputExtraPax);
-        containerDiv.appendChild(newInputPet);
-        containerDiv.appendChild(newInputPackage);
-        containerDiv.appendChild(newInputcheckIn);
-        containerDiv.appendChild(newInputCheckOut);
-        containerDiv.appendChild(newInputDP);
-        containerDiv.appendChild(newInputBalance);
-        containerDiv.appendChild(newInputComment);
-        containerDiv.appendChild(document.createElement('br'));
+        newInputBalance.setAttribute('type', 'text');
+        newInputBalance.setAttribute('name', 'dynamicInputExtraPax_' + i);
+        newInputBalance.setAttribute('id', 'dynamicInputExtraPax_' + i);
+        newInputBalance.setAttribute('placeholder', 'Name: ' + (toJson[1]));
+        newInputBalance.setAttribute('style', 'width:150px; visibility:hidden');
+        newInputBalance.setAttribute('value', (toJson[11]));
+          
+        newInputComment.setAttribute('name', 'dynamicInputComment_' + i);
+        newInputComment.setAttribute('id', 'dynamicInputComment_' + i);
+        newInputComment.setAttribute('placeholder', 'comment');
+        newInputComment.setAttribute('style', 'width:150px; visibility:hidden');        
+        newInputComment.innerHTML = toJson[12];
+
+        newInputDataCreated.setAttribute('type', 'text');
+        newInputDataCreated.setAttribute('name', 'dynamicInputExtraPax_' + i);
+        newInputDataCreated.setAttribute('id', 'dynamicInputExtraPax_' + i);
+        newInputDataCreated.setAttribute('placeholder', 'Name: ' + (toJson[1]));
+        newInputDataCreated.setAttribute('style', 'width:150px; visibility:hidden');
+        newInputDataCreated.setAttribute('value', (toJson[13]));        
+              
+        dynamicInput.appendChild(newInputName);
+        dynamicInput.appendChild(newInputDateIn);
+        dynamicInput.appendChild(newInputDateOut);        
+        dynamicInput.appendChild(newInputPhoneNumber);
+        dynamicInput.appendChild(newInputEmail);
+        dynamicInput.appendChild(newInputPax);
+        dynamicInput.appendChild(newInputExtraPax);
+        dynamicInput.appendChild(newInputPet);
+        dynamicInput.appendChild(newInputPackage);
+        dynamicInput.appendChild(newInputcheckIn);
+        dynamicInput.appendChild(newInputCheckOut);
+        dynamicInput.appendChild(newInputDP);
+        dynamicInput.appendChild(newInputBalance);
+        dynamicInput.appendChild(newInputComment);
+        dynamicInput.appendChild(newInputDataCreated);                
+        dynamicInput.appendChild(document.createElement('br'));
+        dynamicInput.appendChild(document.createElement('br'));
     });
   }
 });
+
+
+
+function createCalendarDateDynamic()
+{
+  for(let day = 1; day <= 31; day++)
+  {
+    const Jan = document.createElement('div');
+
+    Jan.setAttribute("class", "card-schedule");
+    Jan.innerHTML = day;
+    
+    dynamicDatesJan.appendChild(Jan);
+  }
+
+  for(let day = 1; day <= 31; day++)
+  {
+    const Feb = document.createElement('div');
+
+    Feb.setAttribute("class", "card-schedule");
+    Feb.innerHTML = day;
+          
+    dynamicDatesFeb.appendChild(Feb);
+  }  
+
+  for(let day = 1; day <= 31; day++)
+  {
+    const Mar = document.createElement('div');
+
+    Mar.setAttribute("class", "card-schedule");
+    Mar.innerHTML = day;
+          
+    dynamicDatesMar.appendChild(Mar);
+  } 
+
+  for(let Month = 0; Month < 12; Month++)
+  {
+    const dynamicMM = document.createElement('div');    
+
+    dynamicMM.setAttribute("class", "cardDynamicManipulation");
+    dynamicMM.setAttribute("id", "dayHeader");
+    
+    dynamicMonthsDates.appendChild(dynamicMM);    
+
+    const dynamicDates = document.querySelector('#dayHeader');
+    dynamicDates.innerHTML = "";
+
+    const newDynamicMM = document.createElement('div');
+    newDynamicMM.setAttribute("id", "dayHere");
+
+    dynamicDates.appendChild(newDynamicMM);
+
+    for(let day = 0; day < 31; day++)
+    {
+      const createDay = document.createElement('div');
+
+      createDay.setAttribute("class", "card-schedule");
+      createDay.innerHTML = day;
+            
+      dynamicDates.appendChild(createDay);
+    }
+  }
+}
+
+createCalendarDateDynamic();
